8e6ec6e72d74c2181e4fef9f080535acd154c545
Sheikh sazil <sheikhsazil5@gmail.com>
2026-07-28 12:30:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* ===========================
-   Dear Disha
+   Dear Lavanya Das
    Script.js
 =========================== */
 // Falling Petals
@@ -182,7 +182,7 @@ const finalPopup = document.getElementById("finalPopup");
 const finalText = document.getElementById("finalText");
 const closeFinal = document.getElementById("closeFinal");
 
-const finalMessage = `Dear Disha ❤️
+const finalMessage = `Dear Lavanya Das ❤️
 
 Thank you for being one of the most beautiful chapters of my life.
 
@@ -283,7 +283,7 @@ const readMore = document.getElementById("readMore");
 const hiddenLetter = document.getElementById("hiddenLetter");
 const hiddenText = document.getElementById("hiddenText");
 
-const fullLetter = `Dear Disha❤️
+const fullLetter = `Dear Lavanya Das❤️
 
 Some people become memories.
 
@@ -478,7 +478,7 @@ finalMusic.play().catch(()=>{});
 
     setTimeout(()=>{
 
-        alert("🌸 Some memories never fade...\n\nThey bloom forever.\n\n❤️ Thank You, Disha❤️");
+        alert("🌸 Some memories never fade...\n\nThey bloom forever.\n\n❤️ Thank You, Lavanya Das❤️");
 
     },3500);
 
